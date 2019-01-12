173716fa02aa8fed82ae62a8f8e2f93f1fa8a955
Dylan Dusenberry <dylandusenberry975@gmail.com>
2019-01-12 16:51:19 -0600

    update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,7 +13,6 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-
-    products();
+    console.log('connected as id' + connection.threadId);
 });
 
