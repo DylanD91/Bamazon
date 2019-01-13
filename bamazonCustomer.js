d031cacc961b7a0cd100f6defbe00ba5c63d0df7
Dylan Dusenberry <dylandusenberry975@gmail.com>
2019-01-12 22:34:45 -0600

    update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,8 @@
 // Dependencies
-var mysql = require("mysql");
-var inquirer = require("inquirer");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
 
-// This is my MySQL info
+// This is my MySQL info for the DB
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -11,14 +11,22 @@ var connection = mysql.createConnection({
     database: "Bamazon"
 });
 
-// This is where I connect with DB
+// This is where I connect with DB and MySQL server
 connection.connect(function(err) {
     if (err) throw err;
     console.log('connected as id' + connection.threadId);
 });
 
-// I am creating a function here to display products on my Bamazon
-var displayProducts = function() {
-    console.log("Welcome to Dylan's Bamazon! Check out my stores inventory below!");
+// I am creating a function here that shows the details and items that are for sale
+function start() {
+    connection.query("SELECT * FROM products", function(error,response) {
+// If an error occurs, javascript will throw an error
+     if(error) throw err;
+     console.table(results);
+     productID();
+
+    });
 }
+
+
 
--- schema.sql
+++ schema.sql
@@ -2,27 +2,42 @@ USE Bamazon_DB;
 
 CREATE TABLE products (
     item_id INT auto_increment NOT NULL,
-    product_name VARCHAR(50) NOT NULL,
-    department_name VARCHAR(50) NOT NULL,
-    price DECIMAL (10, 4), NOT NULL,
-    stock_quantity INT(70),
+    product_name VARCHAR(100) NOT NULL,
+    department_name INTEGER(50) NOT NULL,
+    price INTEGER (50), NOT NULL,
+    stock_quantity INTEGER(50),
     PRIMARY KEY (item_id)
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Gibson Guitar", "Guitar", 900, 60);
+VALUES ("Gibson Guitar", "String", 900, 60);
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Fender Acoustic", "Guitar", 300, 40);
-
-
-INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Mitchell Ukulele", "Ukulele", 200, 30);
+VALUES ("Fender Acoustic", "String", 300, 40);
 
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
-VALUES ("Ibanez Ukulele", "Ukulele", 400, 50);
+VALUES ("Mitchell Ukulele", "String", 200, 30);
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Ibanez Ukulele", "String", 400, 50);
 
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Yamaha Piano", "Piano", 800, 70);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Aloha Ukulele", "String", 90, 20);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Casio Piano", "Piano", 200, 10);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Ludwig Drums", "Percussion", 500, 40);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Premier Drums", "Percussion", 700, 30);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Rogue Mandolin", "String", 150, 40);
